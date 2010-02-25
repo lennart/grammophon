1b0798582ac098e9bfc5a1964d2a9893c8246cca
lennart <l.melzer@tu-bs.de>
2010-02-25 20:11:16 +0100

    Reverted back to original playdar.js, playdar should extract domain

--- public/playdar.js
+++ public/playdar.js
@@ -29,7 +29,7 @@ Playdar = {
     player: null,
     auth_details: {
         name: window.document.title,
-        website: window.location.hostname
+        website: window.location.protocol + '//' + window.location.host + '/'
     },
     nop: function () {},
     setupClient: function (listeners) {
