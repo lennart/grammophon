e4e7c5c4590bf9bdeefab46e32603180eb4bc5f8
lennart <l.melzer@tu-bs.de>
2010-03-03 12:36:18 +0100

    Slight fix for playdar status styling and handling

--- public/grammophon.js
+++ public/grammophon.js
@@ -1,31 +1,61 @@
-var swfVersionStr = "10.0.0";
-var xiSwfUrlStr = "";
-var flashvars = {};
-var params = {};
-params.quality = "high";
-params.bgcolor = "#ffffff";
-params.allowscriptaccess = "sameDomain";
-params.allowfullscreen = "true";
-var attributes = {};
-attributes.id = "Grammophon";  attributes.name = "Grammophon";
-attributes.align = "middle";
-swfobject.embedSWF(  
-  "public/jukebox/Grammophon.swf", "flashContent",
-  "100%", "550px",
-  swfVersionStr, xiSwfUrlStr,
-  flashvars, params, attributes);
-/* First, set the client up with some callbacks. */
-Playdar.auth_details.type = "flash";
-Playdar.setupClient({
+var GRAMMOPHON = {
 
-    // Called when the browser is authorised to query Playdar.
-    onAuth: function () {
-      // At this point, we can query the Playdar API for a track and start polling for matches.
-      alert("Connected");
-    }
+  update_status: function(message) {
+    document.getElementById("playdarStatus").innerHTML = message;
+  },
+
+  hide_status: function() {
+    var status = document.getElementById("playdarStatus");
+    status.parentNode.removeChild(status);
+  },
+  init: function() {
+
+    /* First, set the client up with some callbacks. */
+    Playdar.USE_STATUS_BAR = false;
+    Playdar.auth_details.receiverurl = Playdar.Util.location_from_url("/playdar_auth.html").href;
+    Playdar.auth_details.type = "flash";
+    Playdar.setupClient({
+        onStat: function (detected) {
+          if(detected) {
+            GRAMMOPHON.update_status(Playdar.client.get_auth_link_html("Connect"));
+          }
+          else {
+            GRAMMOPHON.update_status("No Playdar found");
+          }
+        },
+
+        // Called when the browser is authorised to query Playdar.
+        onAuth: function () {
+          // var disconnect_link = Playdar.client.get_disconnect_link_html(
+          //    STRINGS.disconnect_from_playdar_text
+          //    );
+          //GRAMMOPHON.update_status('<strong>'+Playdar.client.get_disconnect_link_html("Disconnect")+'</strong>');
+
+          //      PLAYDAR.resolve_current_playlist();
+          GRAMMOPHON.hide_status();
+
+          var swfVersionStr = "10.0.0";
+          var xiSwfUrlStr = "";
+          var flashvars = {};
+          var params = {};
+          params.quality = "high";
+          params.bgcolor = "#ffffff";
+          params.allowscriptaccess = "sameDomain";
+          params.allowfullscreen = "true";
+          var attributes = {};
+          attributes.id = "Grammophon";  attributes.name = "Grammophon";
+          attributes.align = "middle";
+          swfobject.embedSWF(  
+            "public/jukebox/Grammophon.swf", "flashContent",
+            "100%", "100%",
+            swfVersionStr, xiSwfUrlStr,
+            flashvars, params, attributes);
+        }
 
 
-});
+    });
 
-/* The client is now ready to check in with Playdar. */
-Playdar.client.go();
+    Playdar.client.go();
+  }
+}
+
